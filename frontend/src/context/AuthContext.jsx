62cb7b8b6632d43323dbe67241ab8367257bab29
Muhammad Shahzeel <shazeelarain786@gmail.com>
2025-05-11 20:58:50 +0500

    added login context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from "react";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext();
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case "LOGIN":
+        return { user: action.payload };
+        case "LOGOUT":
+        return { user: null };
+        default:
+        return state;
+    }
+ 
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  console.log("AuthContext state: ", state);
+  
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
